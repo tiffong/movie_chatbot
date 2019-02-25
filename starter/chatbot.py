3a817495b285566076dcd82c91be1c06aecbfcf7
Gabby <igibzster@gmail.com>
2019-02-25 14:41:58 -0800

    ignore titles in extract sentiment

--- starter/chatbot.py
+++ starter/chatbot.py
@@ -6,7 +6,7 @@ import movielens
 
 import numpy as np
 import re
-from PorterStemmer import PorterStemmer
+# from PorterStemmer import PorterStemmer
 from heapq import nlargest
 import random
 
@@ -27,10 +27,11 @@ class Chatbot:
       self.articles = ['a', 'an', 'the']
 
       sentiment = movielens.sentiment()
-      self.porterStemmer = PorterStemmer()
-      self.sentiment = {}
-      for word in sentiment:
-          self.sentiment[self.porterStemmer.stem(word)] = sentiment[word]
+      # self.porterStemmer = PorterStemmer()
+      self.sentiment = sentiment
+      # self.sentiment = {}
+      # for word in sentiment:
+      #     self.sentiment[self.porterStemmer.stem(word)] = sentiment[word]
 
       self.negation_words = ['no','not','neither','hardly','barely','doesnt','isnt','wasnt','shouldnt','wouldnt',
                              'couldnt','wont',  'cant','dont','didnt','nor','ni','werent']
@@ -296,6 +297,9 @@ class Chatbot:
       :returns: a numerical value for the sentiment of the text
       """
 
+      text = re.sub("([\"]).*?([\"])", "\g<1>\g<2>", text)
+      text = text.replace("\"", "").strip()
+      print(text)
       text = re.sub(r'[^\w\s]', '', text)  # removing punctuation
       text = text.lower()  # lowercase
       words = text.split(' ') # getting individual words
@@ -303,7 +307,7 @@ class Chatbot:
       score = 0
       negate = False
       for word in words:
-          word = self.porterStemmer.stem(word)
+          # word = self.porterStemmer.stem(word)
           if word in self.negation_words:
               negate = True
           elif word in self.sentiment:
